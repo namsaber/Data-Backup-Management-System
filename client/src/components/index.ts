0819937da3ed1896a1d314d87edccd6b52ddec08
Lê Hoài Nam <154336176+namsaber@users.noreply.github.com>
2025-07-03 12:37:05 +0700

    Delete client directory

--- client/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Button } from "./Button";
-export { Input } from "./Input";
-export { Textarea } from "./Textarea";
-export { Modal } from "./Modal";
-export { BackupForm } from "./BackupForm";
-export { BackupFilter } from "./BackupFilter";
-export { BackupTable } from "./BackupTable";
--- client/src/pages/index.ts
+++
@@ -1,3 +0,0 @@
-export { BackupListPage } from "./BackupListPage";
-export { AddBackupPage } from "./AddBackupPage";
-export { EditBackupPage } from "./EditBackupPage";
